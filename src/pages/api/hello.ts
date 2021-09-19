9803b8540b62626cea033a6c0efaff696d53c660
Mario Beltrán Alarcón <belco90@gmail.com>
2021-09-19 15:51:05 +0200

    return cached random string from API

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
+// TODO: temporary method, remove later
+const random = (length = 8): string => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  let str = ''
+  for (let i = 0; i < length; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+
+  return str
+}
+
+const CACHE_SECONDS = 21600 // 6 hours
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ): void {
-  res.status(200).json({ name: 'John Doe' })
+  res.setHeader('Cache-Control', `s-maxage=${CACHE_SECONDS}`)
+  res.status(200).json({ name: random() })
 }
