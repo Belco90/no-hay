0a38b1ffe42118285d69ada5739768a541f37ead
Mario Beltrán Alarcón <belco90@gmail.com>
2021-09-19 14:15:24 +0200

    setup chakra-ui

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme, theme, ThemeOverride } from '@chakra-ui/react'
+
+const themeExtension: ThemeOverride = {
+  colors: {
+    primary: theme.colors.teal,
+    secondary: theme.colors.cyan,
+  },
+  styles: {
+    global: {
+      'html, body, #__next': { height: '100%' },
+    },
+  },
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: true,
+  },
+}
+
+const extendedTheme = extendTheme(themeExtension)
+
+export { extendedTheme as theme }
